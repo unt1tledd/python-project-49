d32f2b3c8ae327e7cf3bd89e7b74545a1413a9a6
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2022-11-12 20:29:15 +0300

    Update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -9,8 +9,8 @@ rule = 'What is the result of the expression?'
 def tasking():
     number1 = random.randint(0, 100)
     number2 = random.randint(0, 100)
-    l = ['+', '-', '*']
-    operation = random.choice(l)
+    operations = ['+', '-', '*']
+    operation = random.choice(operations)
     question = f"{number1} {operation} {number2}"
     correct_answer = str(arithmetic_operations(operation, number1, number2))
     return question, correct_answer
