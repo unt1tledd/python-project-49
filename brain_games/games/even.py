8d648a718c75eb49e73aa34073f8d37f89ea234f
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2022-11-13 21:56:53 +0300

    Update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,15 +6,9 @@ import random
 RULE_OF_GAME = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def is_even(question):
-    check = (question % 2 == 0)
-    return check
-
-
 def tasking():
     question = random.randint(1, 100)
-    check = is_even(question)
-    if check:
+    if question % 2 == 0:
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
