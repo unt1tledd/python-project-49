7e4270a332a7a6a8872ab4f62c0550f7e32dbdde
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2022-11-13 13:07:24 +0300

    Update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -12,5 +12,9 @@ def is_even(question):
 
 def tasking():
     question = random.randint(1, 100)
-    correct_answer = str(is_even(question))
+    check = is_even(question))
+    if check == True:
+        correct_answer = 'yes'
+    else:
+        correct_answer = 'no'
     return question, correct_answer
