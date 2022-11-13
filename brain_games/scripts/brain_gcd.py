7c7300a391d294a31d8d01a26f92eb57865ef6e8
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2022-11-13 21:44:18 +0300

    Update brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,11 +1,11 @@
 
 
-from brain_games.engine import logic_of_game
+from brain_games.engine import play
 from brain_games.games import gcd
 
 
 def main():
-    logic_of_game(gcd)
+    play(gcd)
 
 
 if __name__ == '__main__':
