6e4fc98cef8b3c4a87761f5051cb02c676530c9c
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2022-11-13 22:21:41 +0300

    Update engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -24,6 +24,7 @@ def play(game):
             if a == 0:
                 print(f'Congratulations, {name}!')
         else:
-            print(f"{answer} is wrong answer ;(. Correct answer was {correct_answer}")
+            print(f'{answer} is wrong answer ;(.')
+            print(f'Correct answer was {correct_answer}')
             print(f"Let's try again, {name}!")
             break
