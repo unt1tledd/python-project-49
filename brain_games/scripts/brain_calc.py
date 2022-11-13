2a1d5e31274f99ce9673c843d93fe6ce56ef2536
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2022-11-13 21:43:41 +0300

    Update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,11 +1,11 @@
 
 
-from brain_games.engine import logic_of_game
+from brain_games.engine import play
 from brain_games.games import calc
 
 
 def main():
-    logic_of_game(calc)
+    play(calc)
 
 
 if __name__ == '__main__':
