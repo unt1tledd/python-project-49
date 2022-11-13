587b938b23782102e13f60d5de073c474934048f
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2022-11-13 14:02:09 +0300

    Create prime.py

---
+++ brain_games/games/prime.py
@@ -0,0 +1,20 @@
+
+import random
+
+
+def is_prime(num):
+    b = 0
+    for i in range(2, 9):
+        if (num % i) == 0:
+            b += 1
+    return b
+
+
+def tasking():
+  num = random.randint(2,100)
+  b = is_prime(num)
+  if (num >= 9 and b > 0) or (num <= 9 and b > 1):
+    correct_answer = 'no'
+  else:
+    correct_answer = 'yes'
+  return num, correct_answer
