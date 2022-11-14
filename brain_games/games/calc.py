6bfb539a8cd74a4f00dee76510298345425f295c
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2022-11-14 19:56:46 +0300

    Update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -4,7 +4,7 @@ import random
 RULE_OF_GAME = 'What is the result of the expression?'
 
 
-def count(operation, number1, number2):
+def calculate(operation, number1, number2):
     if operation == '+':
         correct_answer = number1 + number2
     elif operation == '-':
@@ -14,11 +14,11 @@ def count(operation, number1, number2):
     return correct_answer
 
 
-def play_round():
+def generate_round():
     number1 = random.randint(0, 100)
     number2 = random.randint(0, 100)
     operations = ['+', '-', '*']
     operation = random.choice(operations)
     question = f"{number1} {operation} {number2}"
-    correct_answer = str(count(operation, number1, number2))
+    correct_answer = str(calculate(operation, number1, number2))
     return question, correct_answer
