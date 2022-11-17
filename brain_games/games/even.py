c4712aea7dcf1e7dc2b8221a2c8b8f56a926f613
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2022-11-17 08:38:03 +0300

    Update even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,7 +5,7 @@ RULE_OF_GAME = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def is_even(question):
-    return (question % 2 == 0)
+    return question % 2 == 0
 
 
 def generate_round():
