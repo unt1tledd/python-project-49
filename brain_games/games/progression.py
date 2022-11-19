14012c7797941b6aab46110c9569e71efd7a18d9
unt1tledd <113057191+unt1tledd@users.noreply.github.com>
2022-11-19 16:04:12 +0300

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,18 +4,18 @@ import random
 RULE_OF_GAME = 'What number is missing in the progression?'
 
 
-def create_progression():
+def create_progression(num1, num2, st):
     progression = []
-    num1 = random.randint(0, 10)
-    num2 = random.randint(90, 100)
-    st = random.randint(1, 10)
     for i in range(num1, num2, st):
         progression.append(i)
     return progression
 
 
 def generate_round():
-    progression = create_progression()
+    num1 = random.randint(0, 10)
+    num2 = random.randint(90, 100)
+    st = random.randint(1,10)
+    progression = create_progression(num1, num2, st)
     num_to_hide = random.randint(1, 9)
     correct_answer = str(progression[num_to_hide])
     progression[num_to_hide] = '..'
